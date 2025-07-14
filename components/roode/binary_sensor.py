4ee4dca301680db95e557f41321da963a7e87805
ByteRookie <Jasonlightman@gmail.com>
2025-07-14 16:50:18 -0700

    Remove unused xshut state sensor

--- components/roode/binary_sensor.py
+++ components/roode/binary_sensor.py
@@ -12,20 +12,12 @@ from . import Roode, CONF_ROODE_ID
 DEPENDENCIES = ["roode"]
 
 CONF_PRESENCE = "presence_sensor"
-CONF_XSHUT_STATE = "sensor_xshut_state"
-TYPES = [CONF_PRESENCE, CONF_XSHUT_STATE]
+TYPES = [CONF_PRESENCE]
 
 CONFIG_SCHEMA = cv.Schema(
     {
         cv.GenerateID(CONF_ROODE_ID): cv.use_id(Roode),
         cv.Optional(CONF_PRESENCE): binary_sensor.binary_sensor_schema(
-            binary_sensor.BinarySensor
-        ).extend(
-            {
-                cv.GenerateID(): cv.declare_id(binary_sensor.BinarySensor),
-            }
-        ),
-        cv.Optional(CONF_XSHUT_STATE): binary_sensor.binary_sensor_schema(
             binary_sensor.BinarySensor
         ).extend(
             {

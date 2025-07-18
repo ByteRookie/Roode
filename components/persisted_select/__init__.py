76b887d582c72ff1a57b34fb8d35a3aaaf30ad3f
ByteRookie <Jasonlightman@gmail.com>
2025-07-18 08:47:11 -0700

    Fix persisted select schema to allow options

--- components/persisted_select/__init__.py
+++ components/persisted_select/__init__.py
@@ -1,16 +1,30 @@
 import esphome.codegen as cg
 import esphome.config_validation as cv
 from esphome.components import select
-from esphome.const import CONF_ID, CONF_RESTORE_VALUE
+from esphome.const import (
+    CONF_ID,
+    CONF_ICON,
+    CONF_INITIAL_OPTION,
+    CONF_NAME,
+    CONF_OPTIONS,
+    CONF_RESTORE_VALUE,
+)
 
-PersistedSelect = select.select_ns.class_("PersistedSelect", select.Select, cg.Component)
+PersistedSelect = select.select_ns.class_(
+    "PersistedSelect", select.Select, cg.Component
+)
 
-PERSISTED_SELECT_SCHEMA = select.select_schema(PersistedSelect).extend(
+PERSISTED_SELECT_SCHEMA = cv.Schema(
     {
         cv.GenerateID(): cv.declare_id(PersistedSelect),
+        cv.Optional(CONF_NAME): cv.string,
+        cv.Required(CONF_OPTIONS): cv.ensure_list(cv.string),
+        cv.Optional(CONF_ICON): cv.icon,
+        cv.Optional(CONF_INITIAL_OPTION): cv.string,
         cv.Optional(CONF_RESTORE_VALUE, default=True): cv.boolean,
     }
-)
+).extend(cv.COMPONENT_SCHEMA)
+
 
 async def new_persisted_select(config):
     var = await select.new_select(config)

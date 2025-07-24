bbca3631fd90dc667b11c18dbee207ea78d374d5
ByteRookie <Jasonlightman@gmail.com>
2025-07-23 20:28:15 -0700

    Fix duplicate number error by keeping user name

--- components/roode/number.py
+++ components/roode/number.py
@@ -2,7 +2,7 @@ from typing import OrderedDict
 
 import esphome.codegen as cg
 import esphome.config_validation as cv
-from esphome.const import CONF_ICON, CONF_MAX_VALUE, CONF_NAME, CONF_ID
+from esphome.const import CONF_ICON, CONF_MAX_VALUE
 from esphome.cpp_generator import MockObj
 
 from ..persisted_number import PERSISTED_NUMBER_SCHEMA, new_persisted_number
@@ -18,7 +18,7 @@ CONFIG_SCHEMA = cv.Schema(
         cv.GenerateID(CONF_ROODE_ID): cv.use_id(Roode),
         cv.Optional(CONF_PEOPLE_COUNTER): PERSISTED_NUMBER_SCHEMA.extend(
             {
-                cv.Optional(CONF_ICON, default="mdi:counter"): cv.icon,  # new default
+                cv.Optional(CONF_ICON, default="mdi:counter"): cv.icon,
                 cv.Optional(CONF_MAX_VALUE, 10): cv.int_range(-128, 128),
             }
         ),
@@ -28,11 +28,14 @@ CONFIG_SCHEMA = cv.Schema(
 
 async def setup_people_counter(config: OrderedDict, hub: MockObj):
     max_val = config.get(CONF_MAX_VALUE, 10)
-    clean = OrderedDict({k: v for k, v in config.items() if k != CONF_MAX_VALUE and k != CONF_NAME})
-    # Use the ID as the default name so each device gets a unique entity
-    clean[CONF_NAME] = str(config[CONF_ID])
+    clean = OrderedDict(
+        {k: v for k, v in config.items() if k != CONF_MAX_VALUE}
+    )
     counter = await new_persisted_number(
-        clean, min_value=0, step=1, max_value=max_val
+        clean,
+        min_value=0,
+        step=1,
+        max_value=max_val,
     )
     cg.add(hub.set_people_counter(counter))
 

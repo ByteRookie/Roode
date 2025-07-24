81cdc7d00adad75e15b05072f60a8c18358bda1d
ByteRookie <Jasonlightman@gmail.com>
2025-07-23 17:44:51 -0700

    Default persisted number names

--- components/persisted_number/__init__.py
+++ components/persisted_number/__init__.py
@@ -5,7 +5,9 @@ import esphome.config_validation as cv
 from esphome.components import number
 from esphome.const import (
     CONF_ID,
+    CONF_NAME,
     CONF_RESTORE_VALUE,
+    CONF_DISABLED_BY_DEFAULT,
 )
 
 PersistedNumber = number.number_ns.class_(
@@ -16,6 +18,8 @@ PERSISTED_NUMBER_SCHEMA = number.number_schema(PersistedNumber).extend(
     {
         cv.GenerateID(): cv.declare_id(PersistedNumber),
         cv.Optional(CONF_RESTORE_VALUE, default=True): cv.boolean,
+        cv.Optional(CONF_NAME): cv.string,
+        cv.Optional(CONF_DISABLED_BY_DEFAULT, default=False): cv.boolean,
     }
 )
 
@@ -26,6 +30,12 @@ async def new_persisted_number(
     max_value: float,
     step: Optional[float] = None,
 ):
+    config = {
+        **config,
+        CONF_NAME: config.get(CONF_NAME, str(config[CONF_ID])),
+        CONF_RESTORE_VALUE: config.get(CONF_RESTORE_VALUE, True),
+        CONF_DISABLED_BY_DEFAULT: config.get(CONF_DISABLED_BY_DEFAULT, False),
+    }
     config = cv.Schema(PERSISTED_NUMBER_SCHEMA, extra=cv.ALLOW_EXTRA)(config)
     var = await number.new_number(
         config, min_value=min_value, max_value=max_value, step=step
@@ -33,6 +43,5 @@ async def new_persisted_number(
     from esphome.core import CORE
     CORE.component_ids.add(config[CONF_ID].id)
     await cg.register_component(var, config)
-    if CONF_RESTORE_VALUE in config:
-        cg.add(var.set_restore_value(config[CONF_RESTORE_VALUE]))
+    cg.add(var.set_restore_value(config[CONF_RESTORE_VALUE]))
     return var
